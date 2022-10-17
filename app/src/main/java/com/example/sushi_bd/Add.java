a44333900890361b6d96b3f5f373c848bcebf37b
nasty <nastytrifono03@gmail.com>
2022-10-18 00:33:37 +0300

    First_functionals

--- app/src/main/java/com/example/sushi_bd/Add.java
+++ app/src/main/java/com/example/sushi_bd/Add.java
@@ -1,14 +1,103 @@
 package com.example.sushi_bd;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 
 public class Add extends AppCompatActivity {
-
+    String img="";
+    private ImageView imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+        imageButton=findViewById(R.id.AddImage);
+    }
+
+    public void onClickChooseImage(View view)
+    {
+        getImage();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && data!= null && data.getData()!= null)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                Log.d("MyLog","Image URI : "+data.getData());
+                imageButton.setImageURI(data.getData());
+                Bitmap bitmap = ((BitmapDrawable)imageButton.getDrawable()).getBitmap();
+                encodeImage(bitmap);
+
+            }
+        }
+    }
+
+    private void getImage()
+    {
+        Intent intentChooser= new Intent();
+        intentChooser.setType("image/*");
+        intentChooser.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intentChooser,1);
+    }
+
+    private String encodeImage(Bitmap bitmap) {
+        int prevW = 150;
+        int prevH = bitmap.getHeight() * prevW / bitmap.getWidth();
+        Bitmap b = Bitmap.createScaledBitmap(bitmap, prevW, prevH, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            img= Base64.getEncoder().encodeToString(bytes);
+            return img;
+        }
+        return "";
+    }
+
+
+    public  void Add(View v)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(Add.this);
+        builder.setTitle("Добвить")
+                .setMessage("Вы уверены что хотите добавить данные")
+                .setCancelable(false)
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        Next();
+                    }
+                })
+                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog dialog=builder.create();
+        dialog.show();
+    }
+
+    public void Next()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/sushi_bd/Update_Delet.java
+++ app/src/main/java/com/example/sushi_bd/Update_Delet.java
@@ -1,14 +1,151 @@
 package com.example.sushi_bd;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
 
 public class Update_Delet extends AppCompatActivity {
+    ImageView imageView;
+    EditText Name, Price, Compound;
+    String img="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_delet);
+        imageView=findViewById(R.id.UpdateImage);
+
+        Name=findViewById(R.id.UpName);
+
+        Price=findViewById(R.id.UpPrice);
+
+        Compound=findViewById(R.id.UpCompound);
+    }
+
+    private Bitmap getImgBitmap(String encodedImg) {
+        if(encodedImg!=null&& !encodedImg.equals("null")) {
+            byte[] bytes = new byte[0];
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                bytes = Base64.getDecoder().decode(encodedImg);
+            }
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+        }
+        return BitmapFactory.decodeResource(Update_Delet.this.getResources(),
+                R.drawable.zaglushka);
+    }
+
+
+    public void onClickChooseImage(View view)
+    {
+        getImage();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && data!= null && data.getData()!= null)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                Log.d("MyLog","Image URI : "+data.getData());
+                imageView.setImageURI(data.getData());
+                Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                encodeImage(bitmap);
+
+            }
+        }
+    }
+
+    private void getImage()
+    {
+        Intent intentChooser= new Intent();
+        intentChooser.setType("image/*");
+        intentChooser.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intentChooser,1);
+    }
+
+    private String encodeImage(Bitmap bitmap) {
+        int prevW = 150;
+        int prevH = bitmap.getHeight() * prevW / bitmap.getWidth();
+        Bitmap b = Bitmap.createScaledBitmap(bitmap, prevW, prevH, false);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
+        byte[] bytes = byteArrayOutputStream.toByteArray();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            img=Base64.getEncoder().encodeToString(bytes);
+            return img;
+        }
+        return "";
+    }
+    public void Update_bt(Void v)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(Update_Delet.this);
+        builder.setTitle("Изменение")
+                .setMessage("Вы уверены что хотите изменить данные")
+                .setCancelable(false)
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        Next();
+                    }
+                })
+                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog dialog=builder.create();
+        dialog.show();
+
+
+
+
+    }
+
+    public void Delet_bt(Void v)
+    {
+        AlertDialog.Builder builder=new AlertDialog.Builder(Update_Delet.this);
+        builder.setTitle("Удалить")
+                .setMessage("Вы уверены что хотите Удалить данные")
+                .setCancelable(false)
+                .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        Next();
+                    }
+                })
+                .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+        AlertDialog dialog=builder.create();
+        dialog.show();
+    }
+
+    public void Next()
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
