a57ff8b081caf66516da041271447a516d9e1a1a
nasty <nastytrifono03@gmail.com>
2022-10-18 00:09:24 +0300

    Layout

---
+++ app/src/main/java/com/example/sushi_bd/Update_Delet.java
@@ -0,0 +1,14 @@
+package com.example.sushi_bd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Update_Delet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_delet);
+    }
+}
